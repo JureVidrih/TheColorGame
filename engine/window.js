cb1a30a1d319cee46421884eb9d3c61593301490
JureVidrih <jurevidrih@gmail.com>
2018-02-17 20:09:52 +0100

    A fix to a bug with window snapping.

--- engine/window.js
+++ engine/window.js
@@ -384,13 +384,13 @@ var Window = function(panelInstance, windowId) {
                 this.guiWindow.style.left = "0px";
                 this.guiWindow.style.top = "0px";
                 this.setWidth("50%");
-                this.setHeight("100%");
+                this.setHeight(this.panelInstance.getDesktop().getDOMObject().clientHeight);
             } else if(this.isAtRight) {
                 this.cachedWidth = this.getWidth();
                 this.cachedHeight = this.getHeight();
                 this.cachedXBeforeSnap = document.body.clientWidth-this.getWidth();
                 this.setWidth("50%");
-                this.setHeight("100%");
+                this.setHeight(this.panelInstance.getDesktop().getDOMObject().clientHeight);
                 temp = (document.body.clientWidth - this.getWidth());
                 this.guiWindow.style.left = temp + "px";
                 this.guiWindow.style.top = "0px";
